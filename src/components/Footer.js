3fff88d89e960e0a696f1a4060081cc9e2c9571a
robertschaap <robert.schaap@mac.com>
2019-11-13 19:56:39 +0100

    Footer: fix formatted string

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const Footer = (props) => {
+const Footer = ({ user }) => {
+
   return (
     <footer role='contentinfo'>
       <section id='player_stats'>
-        <p>{
-          `${props.user.name}: level
-          ${props.user.level} -
-          ${props.user.xp.current}/
-          ${props.user.xp.goal} XP -
-          ${Math.round(props.user.timesCorrect / props.user.timesTested * 100)}%`
-        }</p>
+        <p>
+          {`
+            ${user.name}: level
+            ${user.level} -
+            ${user.xp.current}/
+            ${user.xp.goal} XP -
+            ${Math.round(user.timesCorrect / user.timesTested * 100)}%
+          `}
+        </p>
       </section>
     </footer>
   );
